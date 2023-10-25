c2b399ec92e20d330843cf85edfba0b2632e4511
npokrandt <npokrandt919@gmail.com>
2023-10-25 14:30:29 -0500

    adds the contact view design

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,6 +1,24 @@
+import { useState } from "react"
+
 const Contact = () => {
+
+    const handleSubmit = () => {
+
+    } 
+    
     return(
-        <h1>Form to enter name, email address and message</h1>
+        <div className="container">
+            <h2 className="mb-4">Contact me by filling out these fields:</h2>
+            <form className="form p-4 mb-4">
+                <label className="form-label">Input Name Here:</label>
+                <input className='form-control' type="text" />
+                <label className="form-label">Input Email Here:</label>
+                <input className='form-control' type="email" />
+                <label className="form-label">Input Message Here:</label>
+                <input className='form-control' type="text" />
+                <button onSubmit={handleSubmit} type="submit" className="btn btn-primary mt-4">Submit</button>
+            </form>
+        </div>
     )
 }
 
