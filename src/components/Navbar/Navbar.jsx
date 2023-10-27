ef31dc1ba431ce44a6ee2c058ced006a3d19fd08
npokrandt <npokrandt919@gmail.com>
2023-10-26 22:42:51 -0500

    adds properly highlighted nav links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,45 +1,15 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = () => {
-    const [aboutLink, setAboutLink] = useState('white')
-    const [portfolioLink, setPortfolioLink] = useState('black')
-    const [contactLink, setContactLink] = useState('black')
-    const [resumeLink, setResumeLink] = useState('black')
-
-    const selected = e => {
-        const name = e.target.name
-        if (name === 'aboutMe'){
-            setAboutLink('selected')
-            setPortfolioLink('not selected')
-            setContactLink('not selected')
-            setResumeLink('not selected')
-        } else if (name === 'portfolio'){
-            setPortfolioLink('selected')
-            setAboutLink('not selected')
-            setContactLink('not selected')
-            setResumeLink('not selected')
-        } else if (name === 'contact'){
-            setContactLink('selected')
-            setAboutLink('not selected')
-            setPortfolioLink('not selected')
-            setResumeLink('not selected')
-        } else {
-            setResumeLink('selected')
-            setAboutLink('not selected')
-            setPortfolioLink('not selected')
-            setContactLink('not selected')
-        }
-    }
 
     return (
         <div className='d-flex justify-content-center'>
             <div className='row w-100'>
-                <Link to='/' onClick={selected} name='aboutMe' className='link col-sm-12 col-md-6 col-lg-3' style={{color: 'white'}}>About Me</Link>
-                <Link to='portfolio' onClick={selected} name='portfolio' className='link col-sm-12 col-md-6 col-lg-3' style={{color: {portfolioLink}}}>Portfolio</Link>
-                <Link to='contact' onClick={selected} name='contact' className='link col-sm-12 col-md-6 col-lg-3' style={{color: {contactLink}}}>Contact</Link>
-                <Link to='resume' onClick={selected} name='resume' className='link col-sm-12 col-md-6 col-lg-3' style={{color: {resumeLink}}}>Resume</Link>
+                <NavLink to='/' className='link col-sm-12 col-md-6 col-lg-3'>About Me</NavLink>
+                <NavLink to='portfolio'  className='link col-sm-12 col-md-6 col-lg-3' >Portfolio</NavLink>
+                <NavLink to='contact' className='link col-sm-12 col-md-6 col-lg-3' >Contact</NavLink>
+                <NavLink to='resume' className='link col-sm-12 col-md-6 col-lg-3' >Resume</NavLink>
             </div>
         </div>
     )
