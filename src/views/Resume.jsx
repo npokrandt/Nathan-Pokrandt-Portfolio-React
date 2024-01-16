aade47b2db33a091f34a20102b868111527e2d36
npokrandt <npokrandt919@gmail.com>
2024-01-16 13:16:49 -0600

    updated resume

--- src/views/Resume.jsx
+++ src/views/Resume.jsx
@@ -1,11 +1,11 @@
-import resume from './Nathan_Pokrandt_Full_Stack_Web_Dev_Resume.pdf'
+import resume from './Nathan_Pokrandt_Resume_Programming.pdf'
 
 const Resume = () => {
     return(
         <div className="container">
             <h2>Download my resume below:</h2>
             <div className="d-flex flex-column align-items-center">
-                <a href={resume} download="Nathan_Pokrandt_Resume" target="_blank" className="fs-5 py-4 my-3">Link to download Resume</a>
+                <a href={resume} download="Nathan_Pokrandt_Resume_Programming" target="_blank" className="fs-5 py-4 my-3">Link to download Resume</a>
                 <div className="card w-75 mb-4">
                     <div className="card-header">
                         <h3>Proficiencies:</h3>
